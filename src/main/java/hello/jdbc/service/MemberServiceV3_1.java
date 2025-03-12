68cd6a0e9df5582fbb1c7b482167a0d9d858f669
shawnchoi8 <bychoise8@gmail.com>
2025-03-12 22:41:53 +0900

    troubleshooting transactions - transaction manager 1

---
+++ src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -0,0 +1,64 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Transaction - Transaction Manager
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV3_1 {
+
+    private final PlatformTransactionManager transactionManager;
+    private final MemberRepositoryV3 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        //transaction start
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            //business logic
+            bizLogic(fromId, toId, money);
+            transactionManager.commit(status); //성공시 commit
+        } catch (Exception e) {
+            transactionManager.rollback(status); //실패시 rollback
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money); //update sql
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money); //update sql
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("exception during transfer");
+        }
+    }
+
+    private static void release(Connection con) {
+        if (con != null) {
+            try {
+                con.setAutoCommit(true); //Connection Pool 고려
+                con.close();
+            } catch (Exception e) {
+                log.info("error", e);
+            }
+        }
+    }
+}
